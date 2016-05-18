804aeef070a703f239719b7e597d091d383e8525
Yuriy Syrovetskiy <cblp@cblp.su>
2016-05-18 16:16:13 +0300

    [+] test builder started

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,50 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+import           Control.Error    (ExceptT(..), fmapL, runExceptT)
+import           Data.Aeson.TH    (defaultOptions, deriveFromJSON)
+import           Data.List        (isSuffixOf)
+import           Data.Maybe       (catMaybes)
+import           Data.Monoid      ((<>))
+import           Data.Traversable (for)
+import qualified Data.Yaml        as Yaml
+import           System.Directory (doesDirectoryExist, getDirectoryContents)
+import           System.FilePath  ((</>))
+import           Test.Tasty       (TestTree, defaultMain, testGroup)
+import           Test.Tasty.HUnit (testCase)
+
+data TestDescription = TestDescription
+deriveFromJSON defaultOptions ''TestDescription
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    tests <- loadTestsFromDirectory "./examples/"
+    defaultMain tests
+
+loadTestsFromDirectory :: FilePath -> IO TestTree
+loadTestsFromDirectory dir = do
+    files <- getDirectoryContents dir
+    testGroup dir . catMaybes <$> for files loadTestsFrom
+  where
+    loadTestsFrom "."   = pure Nothing
+    loadTestsFrom ".."  = pure Nothing
+    loadTestsFrom fileName  = do
+        let filePath = dir </> fileName
+        isdir <- doesDirectoryExist filePath
+        if isdir then
+            Just <$> loadTestsFromDirectory filePath
+        else
+            if ".yaml" `isSuffixOf` fileName then
+                Just <$> testYaml filePath
+            else
+                pure Nothing
+
+testYaml :: FilePath -> IO TestTree
+testYaml file = runScriptToError $ do
+    TestDescription{} <- mkExceptT ((file <>) . (": " <>) . show) $ Yaml.decodeFileEither file
+    pure . testCase file $ pure ()
+
+mkExceptT :: Functor m => (e1 -> e2) -> m (Either e1 a) -> ExceptT e2 m a
+mkExceptT f = ExceptT . fmap (fmapL f)
+
+runScriptToError :: Monad m => ExceptT String m a -> m a
+runScriptToError esma = either error id <$> runExceptT esma
