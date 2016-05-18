018688e872a9c0eea34386fd9380196e0a3675ba
Yuriy Syrovetskiy <cblp@cblp.su>
2016-05-18 16:34:57 +0300

    [+] first test passed!

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,19 +1,22 @@
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 import           Control.Error    (ExceptT(..), fmapL, runExceptT)
-import           Data.Aeson.TH    (defaultOptions, deriveFromJSON)
-import           Data.List        (isSuffixOf)
+import           Data.Aeson.TH    (defaultOptions, deriveFromJSON, fieldLabelModifier)
+import           Data.Char        (isSpace)
+import           Data.List        (dropWhileEnd, isSuffixOf)
 import           Data.Maybe       (catMaybes)
 import           Data.Monoid      ((<>))
 import           Data.Traversable (for)
 import qualified Data.Yaml        as Yaml
 import           System.Directory (doesDirectoryExist, getDirectoryContents)
 import           System.FilePath  ((</>))
+import           System.Process   (readProcess)
 import           Test.Tasty       (TestTree, defaultMain, testGroup)
-import           Test.Tasty.HUnit (testCase)
+import           Test.Tasty.HUnit (assertEqual, testCase)
 
-data TestDescription = TestDescription
-deriveFromJSON defaultOptions ''TestDescription
+data TestDescription = TestDescription{td_in :: String, td_out :: String}
+deriveFromJSON defaultOptions{fieldLabelModifier = drop 3} ''TestDescription
 
 main :: IO ()
 main = do
@@ -40,11 +43,17 @@ loadTestsFromDirectory dir = do
 
 testYaml :: FilePath -> IO TestTree
 testYaml file = runScriptToError $ do
-    TestDescription{} <- mkExceptT ((file <>) . (": " <>) . show) $ Yaml.decodeFileEither file
-    pure . testCase file $ pure ()
+    testDescription <- mkExceptT ((file <>) . (": " <>) . show) $ Yaml.decodeFileEither file
+    pure . testCase file $ testHaskellRepl testDescription
 
 mkExceptT :: Functor m => (e1 -> e2) -> m (Either e1 a) -> ExceptT e2 m a
 mkExceptT f = ExceptT . fmap (fmapL f)
 
 runScriptToError :: Monad m => ExceptT String m a -> m a
 runScriptToError esma = either error id <$> runExceptT esma
+
+testHaskellRepl :: TestDescription -> IO ()
+testHaskellRepl TestDescription{td_in, td_out} = do
+    outRaw <- readProcess "ghc" ["-e", td_in] ""
+    let out = dropWhileEnd isSpace outRaw
+    assertEqual td_in td_out out
